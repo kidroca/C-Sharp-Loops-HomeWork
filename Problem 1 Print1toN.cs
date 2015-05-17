3f7e5f29756cd33d965f01fac584cfdbd68f102c
kidroca <kidroca@gmail.com>
2015-05-17 11:45:12 +0300

    Added assignments 1 and 2

---
+++ Problem 1 Print1toN.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Print1toN
+{
+    static void Main(string[] args)
+    {
+        // Напишете програма, която отпечатва на конзолата числата от 1 до N. Числото N трябва да се чете от стандартния вход.
+
+        Console.Write("Enter Number n= ");
+
+        int n = 
+            int.Parse
+            (Console.ReadLine());
+        
+        while (n >= 1)
+        {
+            Console.Write(n + " ");
+            n--; 
+        }
+    }
+}
+
---
+++ Problem 2 Print1toN not divisible by 3 & 7.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Напишете програма, която отпечатва на конзолата числата от 1 до N,
+        // които не се делят едновременно на 3 и 7. Числото N да се чете от стандартния вход.
+
+        Console.Write("Enter Number n = ");
+        int n = 
+            int.Parse
+            (Console.ReadLine());
+
+        int startInt = 1;
+
+        while (startInt <= n)
+        {
+            if (startInt % 3 == 0)
+            {
+                startInt++;
+                continue;
+            }
+            else if (startInt % 7 == 0)
+            {
+                startInt++;
+                continue;
+            }
+
+            Console.Write(startInt + " ");
+            startInt++;
+        }
+
+    }
+}
+
